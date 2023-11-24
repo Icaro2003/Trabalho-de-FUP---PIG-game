98924a8d16747c8d4b06e228945d21c7fe4d73de
Caio Oliveira <133588078+Caioshin3@users.noreply.github.com>
2023-11-23 21:12:30 -0300

    Update Jogo do Pig.c
    paia

--- Jogo do Pig.c
+++ Jogo do Pig.c
@@ -1,101 +1,236 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <locale.h>
 
-typedef struct {
+typedef struct{
 int Dado_Player;
+int Dado_Player_2;
 int Dado_PC;
+int Dado_PC_2;
 }Dado;
 
-typedef struct {
+typedef struct{
 int Resultado_Player;
 int Resultado_PC;
 }Resultado;
 
-void Lancar_Dado();
-void Lancar_Dado_PC();
+typedef struct{
+int Resultado_Def_Player;
+int Resultado_Def_PC;
+}Definitivo;
 
-int main (){
-Dado dado;
-Resultado resultado;
-int select;
+void Lancar_Dado(Dado *, Resultado *, Definitivo *, int *, int);
+void Lancar_Dado_PC(Dado *, Resultado *, Definitivo *);
+void Segurar_Dado(Dado * ,Resultado *, Definitivo *);
+void Lancar_Dois_Dados(Dado *, Resultado *, Definitivo *, int *);
 
-while(1){
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int opcao, continuar_jogo = 1, vitoria = 0, Dev = 0;
+    Dado dado;
+    Resultado resultado = {0, 0};
+    Definitivo resultado_definitivo = {0, 0};
+    
+
+    srand(time(NULL));
+
+    while (continuar_jogo)
+    {
+        int Soma_Resultado_Def = resultado.Resultado_Player+resultado_definitivo.Resultado_Def_Player;
         
-scanf("%d",&select);
-system("cls");
-switch(select){
-case 1:
-Lancar_Dado(&dado);
+        if(dado.Dado_Player != 1)
+        {
+            printf("Rodada: Player %d - PC %d\n", Soma_Resultado_Def, resultado_definitivo.Resultado_Def_PC);
+            printf("Vitorias: %d\n",vitoria);
+        }
+        else
+        {
+            printf("Rodada: Player %d - PC %d\n", resultado_definitivo.Resultado_Def_Player, resultado_definitivo.Resultado_Def_PC);
+            printf("Vitorias: %d\n",vitoria);
+        }
+        
+        if(resultado_definitivo.Resultado_Def_PC < 100 && resultado_definitivo.Resultado_Def_Player < 100){
+        printf("1. Lançar o dado\n2. Segurar\n3. Encerrar o jogo\n4. Icarus Mode\n");
+        scanf("%d", &opcao);
+        getchar();
+        system("cls");
+        }
+        switch (opcao)
+        {
+        case 1:
+            Lancar_Dado(&dado, &resultado, &resultado_definitivo,&vitoria,&Dev);
+            break;
+        case 2:
+            Segurar_Dado(&dado, &resultado, &resultado_definitivo);
+            Lancar_Dado_PC(&dado, &resultado, &resultado_definitivo);
+            break;
+        case 3:
+            printf("Encerrando o jogo...\n");
+            continuar_jogo = 0;
+            break;
+        case 4:
+          printf("Red Bull te dá asas!\n");
+          Dev = 1;
+          break;       
 
-if(dado.Dado_Player==1){
-resultado.Resultado_Player = 0;
-printf("Zerou!\n");
-Lancar_Dado_PC(&dado,&resultado);
-}
-else if(resultado.Resultado_PC<100){
-resultado.Resultado_PC += dado.Dado_Player;
-}
-else if(resultado.Resultado_Player==100){
-printf("Player ganhou!");
-}
-else if(resultado.Resultado_PC==100){
-printf("Pc ganhou!");
-}
+        default:
+            printf("Opção inválida!\n");
+            break;
+        }
+    }
 
-break;
-}
-}
-
-
-
-
-
-}
-
-Lancar_Dado(Dado Pont){
-
-srand(time(NULL));
-
-Pont.Dado_Player = (rand()%10)+1;
-
-if(Pont.Dado_Player>6){
-printf("| * |\n");
-Pont.Dado_Player = 1;
-}else if(Pont.Dado_Player==2){
-printf("| * * |\n");
-}else if(Pont.Dado_Player==3){
-printf("| * * * |\n");
-}else if(Pont.Dado_Player==4){
-printf("| * * * * |\n");
-}else if(Pont.Dado_Player==5){
-printf("| * * * * * |\n");
-}else if(Pont.Dado_Player==6){
-printf("| * * * * * * |\n");
+    return 0;
 }
 
+void Lancar_Dado(Dado *dado, Resultado *resultado, Definitivo *resultado_definitivo, int * victory, int dev)
+{
+    if(dev == 1){
+    dado->Dado_Player = 2 + rand()%(6-2+1);
+    }
+    else{
+    dado->Dado_Player = 1 + (rand()%6);
+    }
+    
+    if (dado->Dado_Player == 1)
+    {
+        printf("Você tirou 1! Perdeu os pontos da rodada.\n");
+        resultado->Resultado_Player = 0;
+        Lancar_Dado_PC(dado, resultado, resultado_definitivo);
+    }
+    else
+    {
+        resultado->Resultado_Player += dado->Dado_Player;
+        
+        if (dado->Dado_Player > 6)
+        {
+            printf("Você tirou 1! Perdeu os pontos da rodada.\n");
+            resultado->Resultado_Player = 0;
+            return;
+        }
+        
+        printf("Você tirou %d\n", dado->Dado_Player);
+
+        if(resultado->Resultado_Player > 0){
+        resultado_definitivo->Resultado_Def_Player += resultado->Resultado_Player;    
+        }
+        
+                
+        if (resultado_definitivo->Resultado_Def_Player >= 100)
+        {
+            system("cls");
+            printf("Player ganhou!\n");
+            resultado_definitivo->Resultado_Def_PC = 0;
+            resultado_definitivo->Resultado_Def_Player = 0;
+            resultado->Resultado_Player = 0;
+            resultado->Resultado_PC = 0;
+            dado->Dado_Player = 0;
+            dado->Dado_PC = 0;
+           (*victory)++;
+        }else{
+        resultado_definitivo->Resultado_Def_Player = resultado_definitivo->Resultado_Def_Player - resultado->Resultado_Player;
+        }
+    }
 }
 
-Lancar_Dado_PC(Dado PC1, Resultado PC2){
-
-while(PC2.Resultado_PC < 20){
-srand(time(NULL));
-
-PC1.Dado_PC = (rand()%12)+1;
-if(PC1.Dado_PC>6){
-PC1.Dado_PC = 1+rand()%(6-1+1);
-}
-PC2.Resultado_PC += PC1.Dado_PC;
-
-
-if(PC1.Dado_PC == 1){
-  PC2.Resultado_PC = 0;
-  printf("| * |\n");
-  break;
+void Segurar_Dado(Dado *dado, Resultado *resultado, Definitivo *resultado_definitivo)
+{
+    resultado_definitivo->Resultado_Def_Player += resultado->Resultado_Player;
+    resultado->Resultado_Player = 0;
 }
 
+void Lancar_Dado_PC(Dado *dado, Resultado *resultado, Definitivo *resultado_definitivo)
+{
+    printf("Vez do Computador\n");
+
+    while (resultado->Resultado_PC < 100)
+    {
+        dado->Dado_PC = 1 + (rand() % 20);
+        
+        if(dado->Dado_PC > 6){
+        dado->Dado_PC = 2 + rand() % (6-2+1);   
+        }
+        
+        if (dado->Dado_PC == 1)
+        {
+            printf("Computador tirou 1! Perdeu os pontos da rodada.\n");
+            resultado->Resultado_PC = 0;
+            break;
+        }
+        else
+        {
+            resultado->Resultado_PC += dado->Dado_PC;
+            
+            printf("Computador tirou %d\n", dado->Dado_PC);
+
+            if (resultado_definitivo->Resultado_Def_PC >= 100)
+            { 
+                system("cls");
+                printf("Computador ganhou!\n");
+                resultado_definitivo->Resultado_Def_PC = 0;
+                resultado_definitivo->Resultado_Def_Player = 0;
+                resultado->Resultado_Player = 0;
+                resultado->Resultado_PC = 0;
+                dado->Dado_Player = 0;
+                dado->Dado_PC = 0;
+                break;
+            }
+            
+            if (resultado->Resultado_PC >= 20)
+            {
+                printf("O computador decidiu parar\n");
+                resultado_definitivo->Resultado_Def_PC += resultado->Resultado_PC;
+                resultado->Resultado_PC = 0;
+                break;
+            }
+        }
+    }
 }
 
-}
+void Lancar_Dois_Dados(Dado *dado, Resultado *resultado, Definitivo *resultado_definitivo){
 
+dado->Dado_Player = 1 + (rand()%6);
+dado->Dado_Player_2 = 1 + (rand()%6);
+
+if (dado->Dado_Player == 1 || dado->Dado_Player_2 == 1)
+    {
+        printf("Você tirou 1! Perdeu os pontos da rodada.\n");
+        resultado->Resultado_Player = 0;
+        Lancar_Dado_PC(dado, resultado, resultado_definitivo);
+    }
+    else
+    {
+        resultado->Resultado_Player += dado->Dado_Player;
+        resultado->Resultado_Player += dado->Dado_Player_2;
+        
+        printf("Você tirou %d\n", dado->Dado_Player);
+        printf("Você tirou %d\n", dado->Dado_Player_2);
+        
+
+        if(resultado->Resultado_Player > 0){
+        resultado_definitivo->Resultado_Def_Player += resultado->Resultado_Player;    
+        }
+        
+                
+        if (resultado_definitivo->Resultado_Def_Player >= 100)
+        {
+            system("cls");
+            printf("Player ganhou!\n");
+            resultado_definitivo->Resultado_Def_PC = 0;
+            resultado_definitivo->Resultado_Def_Player = 0;
+            resultado->Resultado_Player = 0;
+            resultado->Resultado_PC = 0;
+            dado->Dado_Player = 0;
+            dado->Dado_PC = 0;
+           (*victory)++;
+        }else{
+        resultado_definitivo->Resultado_Def_Player = resultado_definitivo->Resultado_Def_Player - resultado->Resultado_Player;
+        }
+    }
+
+
+
+}
 
